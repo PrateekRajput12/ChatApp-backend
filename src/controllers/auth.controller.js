3fa652445cb01b5a19c0ed40272255f005e7909a
PrateekRajput12 <ficredinn@gmail.com>
2025-02-17 12:26:26 +0530

    Logout and Login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,10 +49,39 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("login route")
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: "Unvalid credentials" })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid Credentials" })
+        }
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePics
+        })
+    } catch (error) {
+        console.log("Erro in login controlller ", error.message);
+        res.status(500).json({ message: "Invalid Server Error" })
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logout route")
+    try {
+        // res.send("logout route")
+        res.cookie("jwt", "", { maxAge: 0 })
+        res.status(200).json({ message: "Logged Out Successfully" })
+    } catch (error) {
+        console.log("Error in logout controller ", error.message);
+        res.status(500).jsoYn({ message: "Internal Server Error" })
+    }
 }
